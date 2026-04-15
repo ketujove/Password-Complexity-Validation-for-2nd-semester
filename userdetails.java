706c50da07318366908f986b8cb21ab0900e8779
Sunidhi Chauhan <iamsunidhi591@gmail.com>
2026-04-15 20:53:28 +0530

    Add files via upload

---
+++ userdetails.java
@@ -0,0 +1,23 @@
+
+public class userdetails {
+    private String username;
+    private String password;
+
+    //using constructor
+    public userdetails(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    // Getter methods
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+}
+
+    
+
